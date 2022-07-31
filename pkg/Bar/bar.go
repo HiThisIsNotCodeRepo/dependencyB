88bb5007d77ff9c7ec873f8e50ed54fe3396e5b6
qinchenfeng <freddy.qin@gmail.com>
2022-07-31 20:40:27 +0800

    Update

--- pkg/Bar/bar.go
+++ pkg/Bar/bar.go
@@ -1,13 +1,5 @@
 package Bar
 
-import (
-	"github.com/chanxuehong/util/math"
-)
-
 func Bar() string {
 	return "Bar"
-}
-
-func Bar2() int64 {
-	return math.Min(1, 2)
 }
--- pkg/Foo/foo.go
+++ pkg/Foo/foo.go
@@ -1,5 +1,13 @@
 package Foo
 
+import (
+	"github.com/chanxuehong/util/math"
+)
+
 func Foo() string {
 	return "Foo"
+}
+
+func Foo2() int64 {
+	return math.Min(1, 2)
 }
