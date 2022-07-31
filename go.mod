8e33f2b80d11a5f547ec6809e52030997aff52c7
qinchenfeng <freddy.qin@gmail.com>
2022-07-31 20:27:43 +0800

    Update

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/qinchenfeng/dependencyB
 
 go 1.18
+
+require github.com/chanxuehong/util v0.0.0-20200304121633-ca8141845b13
--- pkg/Bar/bar.go
+++ pkg/Bar/bar.go
@@ -1,5 +1,13 @@
 package Bar
 
+import (
+	"github.com/chanxuehong/util/math"
+)
+
 func Bar() string {
 	return "Bar"
+}
+
+func Bar2() int64 {
+	return math.Min(1, 2)
 }
